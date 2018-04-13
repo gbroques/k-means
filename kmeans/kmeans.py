4011d232efde66ad8be26261bdf7b2ae89b9980d
G Roques <groques360@gmail.com>
2018-04-12 22:28:37 -0500

    Add method to select initial centroids

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,2 +1,39 @@
+from random import sample
+from typing import List
+
+
 class KMeans:
+    def __init__(self, num_clusters=8):
+        self._num_clusters = num_clusters
+        self.labels_ = None
+
     pass
+
+    def fit(self, data: List[List]) -> 'KMeans':
+        """Cluster the data into k sets.
+
+        Args:
+            data: Data to cluster into k sets.
+
+        Returns:
+            self
+        """
+        centroids = self._select_initial_centroids(data)
+        # repeat
+        #   Form K clusters by assigning each point to its closest centroid
+        #   Recompute the centroid of each cluster
+        # until centroids do not change
+        return self
+
+    def _select_initial_centroids(self, data: List[List]) -> List:
+        """Select K points as initial centroids.
+
+        Uses random initialization.
+
+        Args:
+            data: The dataset.
+
+        Returns:
+            K initial centroids.
+        """
+        return sample(data, self._num_clusters)
--- test/kmeans_test.py
+++ test/kmeans_test.py
@@ -1,8 +1,12 @@
 import unittest
+from random import seed
 from typing import List
+
+from kmeans import KMeans
 
 
 class KMeansTest(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         cls.data = cls.get_two_clusters()
@@ -26,6 +30,22 @@ class KMeansTest(unittest.TestCase):
                 [1, 2],
                 [2, 1],
                 [2, 2]]
+
+    def test_fit(self):
+        seed(1)
+        expected_labels = [1, 1, 1, 0, 0, 0]
+        k_means = KMeans(num_clusters=2)
+        k_means.fit(self.data)
+        self.assertEqual(expected_labels, k_means.labels_)
+
+    def test_select_initial_centroids(self):
+        seed(1)
+        num_clusters = 2
+        expected_initial_centroids = [[-1, -2], [2, 1]]
+        k_means = KMeans(num_clusters=num_clusters)
+        initial_centroids = k_means._select_initial_centroids(self.data)
+        self.assertEqual(expected_initial_centroids, initial_centroids)
+        self.assertEqual(num_clusters, len(initial_centroids))
 
 
 if __name__ == '__main__':
