a2c126bb801bca9501aa71efe8301d6f8aafea43
G Roques <groques360@gmail.com>
2018-04-12 21:51:20 -0500

    Add sample data for tests

--- test/kmeans_test.py
+++ test/kmeans_test.py
@@ -1,8 +1,31 @@
 import unittest
+from typing import List
 
 
 class KMeansTest(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls):
+        cls.data = cls.get_two_clusters()
+
+    @staticmethod
+    def get_two_clusters() -> List[List]:
+        """Two clusters in a 2-dimensional plane.
+              ^
+              | x x
+              |   x
+        <-----+----->
+          o   |
+          o o |
+              v
+        x - Denotes cluster 1
+        o - Denotes cluster 2
+        """
+        return [[-2, -1],
+                [-1, -2],
+                [-2, -2],
+                [1, 2],
+                [2, 1],
+                [2, 2]]
 
 
 if __name__ == '__main__':
